d4c69dd589b99ff7a7fe51f561e9acc7d7e84d81
Adam Martiska <adam.martiska@kistler.com>
2021-04-19 23:29:25 +0200

    One click select is finally working with keyboardShouldPersistTaps

--- ContestScreen.js
+++ ContestScreen.js
@@ -7,14 +7,31 @@ import {
   TouchableOpacity,
   TextInput,
   FlatList,
+  ScrollView,
+  Keyboard,
 } from "react-native";
 import Autocomplete from "react-native-autocomplete-input";
 import { includes, filter } from "ramda";
 
 const ContestScreen = (props) => {
   const [text, setText] = useState("");
-  const data = ["0m4aka", "0m4asb", "0m4aa", "0s4sas"];
+  const data = [
+    "0m4aka",
+    "0m4asb",
+    "0m4aa",
+    "0s4sas",
+    "0m4aaaa",
+    "0m3sa",
+    "0m2sas",
+    "9320oms",
+    "0ms3",
+    "000m",
+    "0mkol",
+    "0mk3",
+  ];
+
   const [filteredItems, setFilteredItems] = useState([]);
+  const [hideResult, setHideResult] = useState(false);
 
   const handleChangedText = (text) => {
     setText(text);
@@ -25,6 +42,7 @@ const ContestScreen = (props) => {
   const handlePressedOption = (selectedItem) => {
     setText(selectedItem);
     setFilteredItems([]);
+    Keyboard.dismiss();
   };
 
   const renderItem = ({ item }) => {
@@ -34,46 +52,39 @@ const ContestScreen = (props) => {
       </TouchableOpacity>
     );
   };
+  const FlatListSeparator = () => <View style={styles.flatlistSeparator} />;
 
   return (
     <View style={styles.container}>
       <View style={{ width: "100%", marginTop: 20 }}>
         <View style={styles.autocompleteContainer}>
-          <Autocomplete
-            data={filteredItems}
-            value={text}
-            onChangeText={handleChangedText}
-            flatListProps={{
-              keyExtractor: (_, idx) => idx,
-              renderItem: ({ item }) => (
-                <TouchableOpacity onPress={() => handlePressedOption(item)}>
-                  <Text>{item}</Text>
-                </TouchableOpacity>
-              ),
-            }}
-          />
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <Autocomplete
+              data={filteredItems}
+              value={text}
+              placeholder="Znacka"
+              style={styles.autocompleteComponent}
+              onChangeText={handleChangedText}
+              inputContainerStyle={{ borderWidth: 0 }}
+              listStyle={styles.listContainer}
+              keyboardShouldPersistTaps="always"
+              flatListProps={{
+                keyboardShouldPersistTaps: "always",
+                keyExtractor: (_, idx) => idx,
+                ItemSeparatorComponent: FlatListSeparator,
+                renderItem: ({ item }) => (
+                  <TouchableOpacity
+                    onPress={() => handlePressedOption(item)}
+                    style={styles.listItem}
+                  >
+                    <Text>{item}</Text>
+                  </TouchableOpacity>
+                ),
+              }}
+            />
+          </ScrollView>
         </View>
       </View>
-      {/* <View style={{ width: "100%" }}>
-        <View
-          style={{
-            width: "100%",
-            borderBottomColor: "#000000",
-            borderBottomWidth: 1,
-          }}
-        >
-          <TextInput onChangeText={(text) => setText(text)} value={text} />
-        </View>
-        {text.length > 0 && (
-          <FlatList
-            style={{ position: "absolute", zIndex: 1, elevation: 1, top: 30 }}
-            data={data}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-            // extraData={selectedId}
-          />
-        )}
-      </View> */}
       <View>
         <View style={{ position: "absolute", top: 50, left: 20 }}>
           <Text>ahoj co ty</Text>
@@ -103,8 +114,34 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     marginTop: 20,
   },
+  autocompleteComponent: {
+    borderRadius: 5,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderColor: "#000",
+    height: 50,
+  },
+  // autocomplete: {
+  //   borderColor: "black",
+  //   borderWidth: 1,
+  //   borderRadius: 3,
+  //   backgroundColor: "green",
+  // },
+  listItem: {
+    height: 40,
+    paddingLeft: 5,
+    justifyContent: "center",
+  },
+  listContainerStyle: {
+    backgroundColor: "yellow",
+    marginVertical: 20,
+  },
   contentContainerStyle: {
     flex: 0,
+  },
+  flatlistSeparator: {
+    borderWidth: 0.5,
+    borderColor: "#ccc",
   },
 });
 
